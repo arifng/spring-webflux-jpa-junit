a40006a8f9a3606424694f8bb50b333503c32a49
Rana <arifng@gmail.com>
2019-09-18 17:28:56 +0600

    initial commit

---
+++ src/main/java/com/arifng/springjunit/SpringJunitApplication.java
@@ -0,0 +1,13 @@
+package com.arifng.springjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJunitApplication.class, args);
+	}
+
+}
